bd1b13bca9c621404901b07f37d4ead863b84b7a
daboynb <you@example.com>
2024-01-12 21:33:08 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+if [ -f /$HOME/swapfile ]  
+then
+    echo "swap file exist"
+else
+    sudo fallocate -l 32G /$HOME/swapfile
+    sudo chmod 600 /$HOME/swapfile
+    sudo mkswap /$HOME/swapfile
+fi
+sudo swapon /$HOME/swapfile
+sudo bin/act -P catthehacker/ubuntu:full-latest
